aae64ea01dc06e9288dc4beb8688eecd49cdd91a
peterdouce <douce_peter@hotmail.com>
2022-06-15 12:56:00 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("03bee870-72dc-41d7-90f2-04666f787d97");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
